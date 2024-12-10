e1f3b20cbfb4cf50feecbf6335206957f715e992
Aboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2024-12-11 00:41:34 +0200

    Update indexes.sql

--- Courses/CS50’s Introduction to Databases with SQL/Optimizing/your.harvard/indexes.sql
+++ Courses/CS50’s Introduction to Databases with SQL/Optimizing/your.harvard/indexes.sql
@@ -1,6 +1,6 @@
-
 CREATE INDEX "enrollments_BY_student_id" ON "enrollments"("student_id");
 CREATE INDEX "enrollments_BY_course_id" ON "enrollments"("course_id");
-CREATE INDEX "courses_BY_department" ON "courses" ("department");
+CREATE INDEX "courses_BY_department" ON "courses" ("department")
+WHERE "semester" = 'Fall 2023' ;
 CREATE INDEX "courses_by_semester" ON "courses"("semester");
 CREATE INDEX "satisfies_BY_course_id" ON "satisfies"("course_id");
