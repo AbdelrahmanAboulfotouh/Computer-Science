b4a20b41089c311fd10d19255793ef60a25601b6
Abdelrahman Aboulfotouh <aboelfotouhcp2023@gmail.com>
2025-03-01 13:41:39 +0200

    Stack using Linked List implementaion

---
+++ Courses/Data structure and algorithms (DSA)/Data structure/stack/stack_using_linkedList.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack{
+private:
+    struct Node{
+        int data{ };
+        Node* next;
+        Node(int val):data(val){};
+    };
+    Node* top{ };
+    int size{ };
+public:
+    Stack(int size):size(size){};
+    void push(int value)
+    {
+
+            Node* new_item = new Node(value);
+            new_item->next = top;
+            top = new_item;
+
+    }
+    int  pop()
+    {
+        assert(top);
+        Node* cur = top;
+        top=top->next;
+        int val = cur->data;
+        delete cur ;
+        return val;
+
+    }
+
+void print()
+{
+        for(auto cur = top ;cur; cur=cur->next)
+            cout<<cur->data<<" ";
+        cout << endl;
+}
+int peek()
+{
+    assert(top);
+    return top->data;
+}
+};
+int main()
+{
+
+    Stack s(3);
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    s.print();
+    cout<<s.peek();
+    return 0;
+}
