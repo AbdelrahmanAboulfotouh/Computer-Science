7d44aa7dac1f3eeb8f9b753701a0380e3df9e1a9
Abdelrahman Aboulfotouh <aboelfotouhcp2023@gmail.com>
2025-04-18 10:59:50 +0200

    Recursion: array maximum element

--- procedural programming  practices/Recursion/Some_easy_problems.cpp
+++ procedural programming  practices/Recursion/Some_easy_problems.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int length_3n_plus_one(int N)
@@ -11,8 +12,24 @@ int length_3n_plus_one(int N)
         return length_3n_plus_one(3*N + 1)+1;
 
 }
+int my_pow(int value,int p = 2)
+{
+    if(p == 0)
+        return 1;
+    return value * my_pow(value,p-1);
+}
+int arr_max(int arr[],int len)
+{
+    if(len == 1)
+        return arr[1];
+    int max_of_rest = arr_max(arr,len-1);
+    return max(arr[len-1],max_of_rest);
+}
 int main()
 {
-    cout<<length_3n_plus_one(10);
+    //int arr[] = {1,8,2,10,3};
+    //cout<<arr_max(arr,5);
+    //cout<<my_pow(7,2);
+    //cout<<length_3n_plus_one(10);
     return 0;
 }
