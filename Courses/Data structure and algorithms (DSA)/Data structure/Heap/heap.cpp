6a547acc78cb5c034fd23f2f4eb6ed93fbdb4762
Abdelrahman Aboulfotouh <aboelfotouhcp2023@gmail.com>
2025-05-25 13:06:24 +0300

    Datastrucures: heap deleation

--- Courses/Data structure and algorithms (DSA)/Data structure/Heap/heap.cpp
+++ Courses/Data structure and algorithms (DSA)/Data structure/Heap/heap.cpp
@@ -10,15 +10,17 @@ public:
         array = new int[capacity];
         size = 0;
     }
-    int parent(int child){};
+    int parent(int child){
+        return log(child-1)/2;
+    }
     int left_child(int node){
         int pos = 2*node +1 ;
         return pos >=size ? -1 : pos;
-    };
+    }
     int right_child(int node){
         int pos = 2*node+2;
         return pos >= size ? -1 : pos ;
-    };
+    }
     void push(int val){
         assert(size+1 <= capacity);
         array[size++] = val;
@@ -35,6 +37,27 @@ public:
     {
         assert(size > 0);
         return array[0];
+    }
+    void heapify_down(int parent_idx)
+    {
+        int Child = left_child(parent_idx);
+        int Right_child = right_child(parent_idx);
+        if(Child == -1)
+            return;
+        if(Child != -1 and array[Child] > array[Right_child])
+            Child = Right_child;
+        if(array[parent_idx] > array[Child])
+        {
+            swap(array[parent_idx],array[Child]);
+            heapify_down(Child);
+
+        }
+    }
+    void pop()
+    {
+        assert(size > 0);
+        array[0] = array[--size];
+        heapify_down(0);
     }
     ~heap()
     {
