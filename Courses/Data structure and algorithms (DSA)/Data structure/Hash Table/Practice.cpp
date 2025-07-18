e6a57162daaa92f9c32eed8d63ba24e199764505
Abdelrahman Aboulfotouh <aboelfotouhcp2023@gmail.com>
2025-07-18 12:27:20 +0300

    Datastructures: Hash table : practice : getting Common substrings

--- Courses/Data structure and algorithms (DSA)/Data structure/Hash Table/Practice.cpp
+++ Courses/Data structure and algorithms (DSA)/Data structure/Hash Table/Practice.cpp
@@ -10,9 +10,31 @@ int count_unique_substrings(const string &str)
     return S.size();
 
 }
+unordered_set<string> unique_substrings(const string &str)
+{
+    unordered_set<string>S;
+    for(int i{0};i<(int)str.size();++i)
+        for(int len = i ; len < (int)str.size();++len)
+            S.insert(str.substr(i,len - i +1));
+
+    return S;
+
+}
+int count_substrings_match(const string &str1, const string &str2)
+{
+    auto S1 = unique_substrings(str1);//O(N^2 * L)
+    auto S2 = unique_substrings(str2);
+    unordered_set<string> ans;
+    for(auto &W1:S1)
+        for(auto &W2:S2)
+            if(W1 == W2)
+                ans.insert(W1);
+    return ans.size();
+}
 int main()
 {
-    string test = "abcdef";
-    cout<<count_unique_substrings(test);
+    string test1 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+    string test2 = "xaaaaaaaaaaaaaaaaadvsfffffffffffffffffffffffffffffffffffffffffffffffffffffffddddddddddddddddeeeeeey";
+    cout<<count_substrings_match(test1,test2)<<"\n";
     return 0;
 }
