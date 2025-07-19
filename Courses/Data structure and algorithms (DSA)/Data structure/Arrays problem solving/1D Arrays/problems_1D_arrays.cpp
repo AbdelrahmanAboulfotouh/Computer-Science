cc4c760270a7e20e9c5b877c7281b8a8103b0e48
Abdelrahman Aboulfotouh <aboelfotouhcp2023@gmail.com>
2025-07-19 09:11:58 +0300

    Datastructures: 1D Arrays problem solving

--- Courses/Data structure and algorithms (DSA)/Data structure/Arrays problem solving/1D Arrays/problems_1D_arrays.cpp
+++ Courses/Data structure and algorithms (DSA)/Data structure/Arrays problem solving/1D Arrays/problems_1D_arrays.cpp
@@ -40,9 +40,91 @@ void frequancy_the_array(int arr[],int n)
     for(auto & F:fr)
         cout<<F.first<<" Repeated: "<<F.second<<"\n";
 }
+bool is_increasing(int arr[],int n)
+{
+    for(int i{1};i<n;++i)
+        if(arr[i] <= arr[i-1])
+            return false;
+    return true;
+}
+void Replace_min_max(int *arr,int n )
+{
+    int Max = INT32_MIN;
+    int Min = INT32_MAX;
+    for(int i{0};i<n;++i)
+    {
+        Max= max(Max,arr[i]);
+        Min = min(Min,arr[i]);
+    }
+    for(int i{0};i<n;++i)
+    {
+        if(arr[i] == Max)
+        {
+            arr[i] = Min;
+            continue;
+        }
+        if(arr[i] == Min)
+        {
+            arr[i] = Max;
+            continue;
+        }
+    }
+
+}
+vector<int> unique_numbers_orderd()
+{
+    int n;
+    cin>>n;
+    int x{-1},y{-1};
+    bool f = false;
+    vector<int>list;
+    while (n--)
+    {
+        if(!f) {
+            cin >> x;
+            f = true;
+            continue;
+        }
+        cin>>y;
+        if(x != y )
+        {
+            if(n == 0)
+            {
+                list.push_back(y);
+                continue;
+            }
+            list.push_back(x);
+            x = y;
+        }
+
+
+    }
+    return list;
+}
+bool is_palindrome(int arr[],int n)
+{
+    for(int i{0};i<=n/2;++i)
+        if(arr[i] != arr[n-1-i])
+            return false;
+    return true;
+}
+int minimum_pair_equation(int arr[],int n)
+{
+    int ans = INT32_MAX;
+    for(int i{0};i<n-1;++i)
+    {
+        for(int j = i + 1 ; j< n ; ++j)
+        {
+            int tmp = arr[i] + arr[j] + j-i;
+            ans = min(ans,tmp);
+
+        }
+    }
+    return ans;
+}
 int main()
 {
-    int arr[] = {1,1,3,3,5,6};
-    frequancy_the_array(arr,6);
+    int arr[] = {20,1,9,4};
+    cout<<minimum_pair_equation(arr,4);
     return 0;
 }
